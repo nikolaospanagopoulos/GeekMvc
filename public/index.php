3447ddfea1b7b1f61db9f4ccaedb68b70fd95505
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2023-04-23 21:01:44 +0300

    better url for dev server

--- public/index.php
+++ public/index.php
@@ -12,12 +12,20 @@ $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('{controller}/{id:\d+}/{action}/{idex:\d+}/{actionx}');
 
+/**
+ *Get the query string for both XAMPP and php dev server
+ *@return string || null
+ **/
 function getServerQueryString()
 {
 	if (isset($_SERVER['QUERY_STRING'])) {
 		return $_SERVER['QUERY_STRING'];
 	} elseif (isset($_SERVER['REQUEST_URI'])) {
 		$str = $_SERVER['REQUEST_URI'];
+		//remove last char for query string to be valid
+		if (substr($str, -1) == '/') {
+			$str = rtrim($str, '/');
+		}
 		return ltrim($str, '/');
 	}
 

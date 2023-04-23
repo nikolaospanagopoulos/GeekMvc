33eee53c5ac964b3bd7af9ba63ce1a46838f28e9
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2023-04-23 07:10:36 +0300

    more advanced router and fix url issue with dev server

--- public/index.php
+++ public/index.php
@@ -1,19 +1,32 @@
 <?php
 
+use Core\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Core\Application;
 
-$app = new Application();
 
-$router = $app->router;
+$router = new Router();
 
-$router->add('', ['controller' => "Home", 'action' => "index"]);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+$router->add('{controller}/{id:\d+}/{action}/{idex:\d+}/{actionx}');
 
-$url = $_SERVER['QUERY_STRING'];
-echo $url;
-if ($router->match($url)) {
+function getServerQueryString()
+{
+	if (isset($_SERVER['QUERY_STRING'])) {
+		return $_SERVER['QUERY_STRING'];
+	} elseif (isset($_SERVER['REQUEST_URI'])) {
+		$str = $_SERVER['REQUEST_URI'];
+		return ltrim($str, '/');
+	}
+
+	return null;
+}
+
+$uri = getServerQueryString();
+echo $uri;
+if ($router->match($uri)) {
 	echo "<pre>";
 	var_dump($router->getParams());
 	echo "</pre>";
